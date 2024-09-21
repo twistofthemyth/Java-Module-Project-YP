5f8a7d72491a3f5222d26f69decf44721d172e5c
twistofthemyth <twist.of.the.myth@gmail.com>
2024-09-22 01:02:15 +0300

    RaceRunner: исправление обработки некорректного ввода скорости

--- src/main/java/RaceRunner.java
+++ src/main/java/RaceRunner.java
@@ -12,25 +12,43 @@ public class RaceRunner {
         Scanner scanner = new Scanner(System.in);
 
         for (int i = 1; i <= participantsAmount; i++) {
-            System.out.printf("— Введите название машины №%s:\n", i);
-            String participantName = scanner.next();
-            int participantSpeed = -1;
-            do {
-                System.out.printf("— Введите скорость машины №%s:\n", i);
-                participantSpeed = scanner.nextInt();
-                if (!isSpeedCorrect(participantSpeed)) {
-                    System.out.println("— Неправильная скорость\n");
-                }
-            } while (!isSpeedCorrect(participantSpeed));
-
+            String participantName = readParticipantName(scanner, i);
+            int participantSpeed = readParticipantSpeed(scanner, i);
             Automobile participant = new Automobile(participantName, participantSpeed);
             race.addAutomobile(participant);
         }
 
+        scanner.close();
         System.out.println("Самая быстрая машина: " + race.getLeader().getName());
+    }
+
+    private String readParticipantName(Scanner scanner, int participantNumber) {
+        System.out.printf("— Введите название машины №%s:\n", participantNumber);
+        return scanner.nextLine();
+    }
+
+    private int readParticipantSpeed(Scanner scanner, int participantNumber) {
+        while (true) {
+            System.out.printf("— Введите скорость машины №%s:\n", participantNumber);
+            String speedInput = scanner.nextLine();
+            try {
+                int speed = Integer.parseInt(speedInput);
+                if (isSpeedCorrect(speed)) {
+                    return speed;
+                } else {
+                    printWrongInputSpeed();
+                }
+            } catch (NumberFormatException e) {
+                printWrongInputSpeed();
+            }
+        }
     }
 
     private boolean isSpeedCorrect(int speed) {
         return speed >= 0 && speed <= 250;
+    }
+
+    private void printWrongInputSpeed() {
+        System.out.println("— Неправильная скорость\n");
     }
 }
